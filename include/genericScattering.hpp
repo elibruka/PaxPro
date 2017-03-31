d274b8a4327f9195e5176719ee6a0a190503c20c
David Kleiven <davidkleiven446@gmail.com>
2017-03-31 18:21:33 +0200

    Cosmetic change

--- include/genericScattering.hpp
+++ include/genericScattering.hpp
@@ -32,12 +32,23 @@ public:
   unsigned int exportNx{512};
   unsigned int exportNy{512};
   unsigned int FFTPadLength{1024};
-  double xmin{-1.0}, xmax{1.0}, ymin{-1.0}, ymax{1.0}, zmin{-1.0}, zmax{1.0};
-  double dx{0.1}, dy{0.1}, dz{0.1};
-  unsigned int downSampleX{1}, downSampleY{1}, downSampleZ{1};
+  double xmin{-1.0};
+  double xmax{1.0};
+  double ymin{-1.0};
+  double ymax{1.0};
+  double zmin{-1.0};
+  double zmax{1.0};
+  double dx{0.1};
+  double dy{0.1};
+  double dz{0.1};
+  unsigned int downSampleX{1};
+  unsigned int downSampleY{1};
+  unsigned int downSampleZ{1};
   double wavelength{0.1569};
-  double intensityMin{0.0}, intensityMax{1.0};
-  double phaseMin{-3.14159}, phaseMax{3.14159};
+  double intensityMin{0.0};
+  double intensityMax{1.0};
+  double phaseMin{-3.14159};
+  double phaseMax{3.14159};
   std::string imgname{""};
   bool realTimeVisualization{false};
 private:
