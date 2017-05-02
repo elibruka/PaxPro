463907a29072759410f3c077039df936259b0577
David Kleiven <davidkleiven446@gmail.com>
2017-05-02 16:58:13 +0200

    Renamed the directory

---
+++ pypaxpro/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="pypaxpro",
+    version="1.0",
+    description="PyPaxPro Python interface",
+    author="David Kleiven",
+    author_email="davidkleiven446@gmail.com",
+    packages=["pypaxpro"],
+    
+)
