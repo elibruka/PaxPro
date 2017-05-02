2492c1741eb7b161e7b77d606c4deb0fe0e9e8e3
David Kleiven <davidkleiven446@gmail.com>
2017-05-02 17:45:16 +0200

    Setup script seems to work

--- pypaxpro/setup.py
+++ pypaxpro/setup.py
@@ -1,5 +1,20 @@
 from distutils.core import setup, Extension
 
+cppModule = Extension("_pypaxpro", include_dirs=["include"],
+swig_opts=["-c++","-py3","-I./include"],
+sources=["src/absorber.cpp", "src/cladding.cpp", "src/controlFile.cpp", "src/crancNicholson.cpp",
+"src/exitFieldSource.cpp", "src/farFieldPost.cpp", "src/fftSolver2D.cpp", "src/fftSolver3D.cpp", "src/fixedValuesSource.cpp",
+"src/gaussianBeam.cpp", "src/genericScattering.cpp", "src/h5Attribute.cpp", "src/linearMap1D.cpp", "src/paraxialEquation.cpp",
+"src/paraxialSimulation.cpp", "src/paraxialSource.cpp", "src/planeWave.cpp", "src/postProcessing.cpp", "src/postProcessMod.cpp",
+"src/refractiveIndex.cpp", "src/solver.cpp", "src/solver1D.cpp", "src/solver2D.cpp", "src/solver3D.cpp", "src/stdFDsolver.cpp",
+"src/transmittivity.cpp", "src/waveGuideFDSimulation.cpp", "src/borderTracker.cpp", "src/curvedWaveGuide2D.cpp",
+"src/alternatingDirectionSolver.cpp", "src/tetraGeometry.cpp", "src/bvhTree.cpp", "pypaxpro/pypaxpro.i"],
+libraries=["armadillo","jsoncpp","hdf5_hl_cpp","lapack","gsl","gslcblas","visa","sfml-window","sfml-graphics",
+"fftw3_omp"],
+extra_compile_args=["-fopenmp"],
+library_dirs=["/usr/local/lib","/usr/lib", "/usr/lib64"]
+)
+
 setup(
     name="pypaxpro",
     version="1.0",
@@ -7,5 +22,5 @@ setup(
     author="David Kleiven",
     author_email="davidkleiven446@gmail.com",
     packages=["pypaxpro"],
-    ext_modules=Extension("_pypaxpro", ["pypaxpro.i"], include_dirs="include", )
-)
+    ext_modules=[cppModule]
+    )
