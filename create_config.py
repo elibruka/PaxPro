076b53088d7d25e3ba65f7afcd6bed1fd15770d6
David Kleiven <david.kleiven@ntnu.no>
2018-09-03 15:52:06 +0200

    Improving build

---
+++ create_config.py
@@ -0,0 +1,44 @@
+import sys
+
+outfile = "pypaxpro/config.sh"
+def main(argv):
+    libs = []
+    include_dirs = []
+    for item in argv:
+        if item.find(".so") != -1:
+            libs.append(item)
+        elif item.find("/") != -1:
+            include_dirs.append(item)
+
+    unique_paths = []
+    link_lib = []
+    for lib in libs:
+        path = lib.rpartition("/")[0]
+        if path not in unique_paths:
+            unique_paths.append(path)
+        link_arg = "-l" + lib.rpartition("/")[-1].rpartition("lib")[-1]
+        link_arg = link_arg.replace(".so", "")
+        link_lib.append(link_arg)
+
+    with open(outfile, 'w') as out:
+        out.write("LIB_PATH=")
+        for path in unique_paths:
+            out.write("{}:".format(path))
+        out.write("\n")
+
+        out.write("LIB_LINK=\"")
+        for lib in link_lib:
+            out.write("{} ".format(lib))
+        out.write("\"\n")
+
+        out.write("INC_ARG=\"")
+        for inc in include_dirs:
+            out.write("-I{} ".format(inc))
+        out.write("\"\n")
+    print("Configuration written to {}".format(outfile))
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- pypaxpro/createPythonModule.sh
+++ pypaxpro/createPythonModule.sh
@@ -1,6 +1,16 @@
 # Create swig module
 
+INCLUDE_DIR=$1
+LIBS=$2
+echo "Creating python module..."
+source config.sh
+export LIBRARY_PATH=${LIB_PATH}
+echo "Library path:"
+echo ${LIB}
+echo "Include arguments: "
+echo ${INC_ARG}
+echo "Link arguments: "
+echo ${LIB_LINK}
 swig -modern -I../include -c++ -python pypaxpro.i
-c++ -fPIC -c pypaxpro_wrap.cxx -I/usr/include/python3.5m/ -I../include
-c++ -shared pypaxpro_wrap.o ../src/libpaxpro.a -L/usr/local/lib $(python3-config --ldflags) -ljsoncpp -lhdf5_hl_cpp \
- -llapack -lgsl -lgslcblas -lvisa -lsfml-window -lsfml-graphics -lfftw3 -larmadillo -o _pypaxpro.so
+c++ -fPIC -std=c++11 -c pypaxpro_wrap.cxx ${INC_ARG}
+c++ -shared pypaxpro_wrap.o ../src/libpaxpro.a -L/usr/local/lib $(python3-config --ldflags) ${LIB_LINK}
